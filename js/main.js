ef1a628bc736cf8c4fbf0ce32e1fa1611a354693
Nikolay Evdokiomov <89991057+F1vne@users.noreply.github.com>
2023-02-20 22:06:25 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,51 +7,33 @@ headerBurger.addEventListener('click', () => {
     body.classList.toggle("body--lock");
 });
 
-const form          = document.querySelector(".footer__form");
-const submitBtn     = form.querySelector(".footer__form-btn");
-const inputArr      = Array.from(form);
-const validInputArr = [];
+const form  = document.querySelector(".footer__form");
+const email = document.querySelector(".footer__form-input");
+const emailError = document.querySelector('.error');
 
-inputArr.forEach((el) => {
-	if (el.hasAttribute("data-reg")) {
-		el.setAttribute("is-valid", 0);
-		validInputArr.push(el);
-	};
+email.addEventListener('input', function (event) {
+  if (email.validity.valid) {
+    emailError.textContent = ''; 
+    emailError.className = 'error';
+  } else {
+    showError();
+  }
 });
 
-form.addEventListener("input", inputHandler);
-submitBtn.addEventListener("click", btnHandler);
+form.addEventListener('submit', function (event) {
+  if(!email.validity.valid) {
+    showError();
+    event.preventDefault();
+  }
+});
 
-function inputHandler({target}) {
-	if(target.hasAttribute("data-reg")) {
-		inputCheck(target);
-	};
-};
-
-function btnHandler(e) {
-	const isAllValid = [];
-	validInputArr.forEach((el) => {
-		isAllValid.push(el.getAttribute("is-valid"));
-	});
-	const isValid = isAllValid.reduce((acc, currunet) => {
-		return acc && currunet;
-	});
-	if(!Boolean(Number(isValid))) {
-		e.preventDefault();
-	}
-};
-
-function inputCheck(el) {
-	const inputValue = el.value;
-	const inputReg   = el.getAttribute("data-reg");
-	const reg        = new RegExp(inputReg);
-
-
-	if(reg.test(inputValue)) {
-		el.style.backgroundColor = "#BAE399";
-		el.setAttribute("is-valid", 1);
-	} else {
-		el.style.backgroundColor = "#da6969";
-		el.setAttribute("is-valid", 0);
-	};
-};
+function showError() {
+  if(email.validity.valueMissing) {
+    emailError.textContent = 'You need to enter an e-mail address.';
+  } else if(email.validity.typeMismatch) {
+    emailError.textContent = 'Entered value needs to be an e-mail address.';
+  } else if(email.validity.tooShort) {
+    emailError.textContent = `Email should be at least ${ email.minLength } characters; you entered ${ email.value.length }.`;
+  }
+  emailError.className = 'error active';
+}
